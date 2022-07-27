557065e4c82f66a0ac196b09e21e7b5de063c25b
Jack Atkerson <jatkerson18@gmail.com>
2022-07-26 21:17:17 -0500

    Started variables

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment code here
-
+function generatePassword() {
+  var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '_', '^', '{', '|', '}', '~'];
+  var numericCharacters = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var uppercaseCharacters = [];
+  var lowercaseCharacters = [];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
